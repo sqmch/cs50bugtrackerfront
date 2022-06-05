1add24fd0beab3dad2327b2d0cdaa3a759e5e343
sqmch <sqmchh@gmail.com>
2022-06-05 11:00:29 +0100

    Started on README.md, design tweaks to Home.vue

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# Vue 3 + Vite
+# bugtrvcker - cs50x Final Project
+---
+#### Video Demo:  www.youtube.com/myvid24254
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+#### Description:
 
-## Recommended IDE Setup
+##### What is bugtrvcker
+bugtrvcker is a web app for tracking bugs. The user can sign up with an email and password, sign in into their account, create projects and add bugs to projects for convenient tracking. The user can create, read, update and delete projects and tracked bugs.
 
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+##### What was it made with - summary
+bugtrvcker's frontend is written using the Vue.js framework with Quasar as a component library. The backend is written in Python, using FastAPI as a more modern alternative to Flask for serving API endpoints to the frontend, as well as sqlite3 with SQLAlchemy for the database.
+
+##### Frontend
+I chose Vue 3 due to some previous experience with Vue as a very useful tool for modern web development. Instead of re-inventing a lot of wheels, I chose Quasar as my framework on top of Vue for easy access to organized components and styling. Quasar in this project is used similarly to how one would use Bootstrap. Some noteworthy Vue libraries used are VueRouter for routing between pages/components, Pinia for state management, axios for HTTP requests.
+
+Important files:
+- Login.vue - Vue component containing the login page
+- Register.vue - Vue component containing the signup page
+- Hone.vue - Vue component containing the home page with a list of user created projects
+- Project.vue - Vue component containing the list of bugs for a specified project
+- App.vue - Wrapper component for other components, content dynamically changes based on current route
+- index.html - Unlike traditional web development, index.html only exists as an almost blank file where the Vue framework will dynamically inject all relevant code from above components automatically.
+main.js - the Vue app instance itself is created here, as well as imports for some libraries and route details for VueRouter
+
+There are a number of additional config files such as vite.config.js, quasar-variables.sass etc. that get auto-generated and need minimal interaction from the developer unless something relevant needs to be configured differently from default values.
+
+##### Backend
+
+I chose FastAPI as my backend web framework. It's similar enough to flask, but is faster, more convenient and more organized for building API-s due to good support for Python type hints.
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,12 +2,12 @@
 import { ref } from "vue";
 
 const projects = ref([
-  { title: "Lol", description: "Lollergoggles" },
-  { title: "AGagg", description: "jgkgkgkgkgk" },
-  { title: "srwtwtwt", description: "" },
-  { title: "aseasese", description: "ssdfjdfjdfdfjjdf" },
-  { title: "Lol", description: "Lollergoggles" },
-  { title: "AGagg", description: "jgkgkgkgkgk" },
+  { title: "wrkout", description: "Lollergoggles" },
+  { title: "trvcker", description: "jgkgkgkgkgk" },
+  { title: "rdb", description: "" },
+  { title: "portfolio project", description: "ssdfjdfjdfdfjjdf" },
+  { title: "treeclassifier", description: "Lollergoggles" },
+  { title: "wschat", description: "jgkgkgkgkgk" },
   { title: "srwtwtwt", description: "erteryeruerueru" },
   { title: "aseasese", description: "" },
   { title: "Lol", description: "" },
@@ -19,15 +19,20 @@ const projects = ref([
   { title: "aseasese", description: "ssdfjdfjdfdfjjdf" },
 ]);
 const hoveringDescription = ref(false);
+const visible = ref(false);
+const addButtonIcon = ref(true);
+const title = ref("");
 </script>
 <template>
-  <q-layout>
+  <q-layout class="page">
     <q-page-container>
       <q-page>
-        <div class="q-pa-md page">
+        <div class="q-pa-md">
           <div class="row">
             <div class="col-md-2"></div>
+            <!--MAIN COLUMN-->
             <div class="col-md-8">
+              <!--HEADER ROW-->
               <div class="row header items-center justify-center">
                 <h4>projects</h4>
               </div>
@@ -35,11 +40,44 @@ const hoveringDescription = ref(false);
                 <div class="col-6">{{ projects.length }} projects</div>
 
                 <div class="col-6">
-                  <q-btn class="toolbarAdd" flat icon="add">add project</q-btn>
+                  <q-btn
+                    class="toolbarAdd"
+                    flat
+                    :icon="addButtonIcon ? 'add' : 'arrow_upward'"
+                    @click="
+                      visible = !visible;
+                      addButtonIcon = !addButtonIcon;
+                    "
+                    >add project</q-btn
+                  >
                 </div>
               </div>
               <q-separator spaced></q-separator>
+              <!--PROJECT GRID-->
               <div class="row">
+                <div class="col-6"></div>
+                <div class="col-md-6 col-xs-12">
+                  <q-slide-transition>
+                    <div v-show="visible">
+                      <div class="logininput">
+                        <q-input
+                          color="black"
+                          v-model="title"
+                          filled
+                          type="text"
+                          hint="Project title"
+                        />
+                      </div>
+                      <div class="logininput">
+                        <q-btn
+                          to="/register"
+                          class="loginbtn"
+                          label="Add project"
+                        />
+                      </div>
+                    </div>
+                  </q-slide-transition>
+                </div>
                 <div
                   v-for="project in projects"
                   v-bind:key="project"
@@ -52,18 +90,29 @@ const hoveringDescription = ref(false);
                       <div class="text-h6">{{ project.title }}</div>
                     </q-card-section>
 
-                    <q-separator dark />
-
                     <q-card-actions>
-                      <q-btn flat>Open</q-btn>
+                      <q-btn icon="open_in_new" flat color="white">
+                        <q-tooltip> Open project </q-tooltip></q-btn
+                      >
                       <q-space></q-space>
-                      <q-btn flat>Delete</q-btn>
+
+                      <q-btn flat color="white" icon="delete"
+                        ><q-tooltip> Delete project </q-tooltip></q-btn
+                      >
                     </q-card-actions>
                   </q-card>
                 </div>
               </div>
               <q-page-sticky position="bottom-right" :offset="[18, 18]">
-                <q-btn fab icon="add" class="addfab" />
+                <q-btn
+                  fab
+                  icon="add"
+                  class="addfab"
+                  @click="
+                    visible = !visible;
+                    addButtonIcon = !addButtonIcon;
+                  "
+                />
               </q-page-sticky>
             </div>
             <div class="col-md-2"></div>
@@ -73,12 +122,15 @@ const hoveringDescription = ref(false);
 <style lang="sass" scoped>
 .page
   background-color: #D9D9D9
+  height: 100%
 .projectcard
-  background-color: #F76F72
+  cursor: pointer
+  background-color: #fbb7b8//#F76F72
   margin: 10px
   color: black!important
+  transition: all .1s ease-in-out
   &:hover
-    color: yellow
+    transform: scale(1.1)
 .addfab
   background-color: white
   height: 60px
@@ -96,7 +148,15 @@ const hoveringDescription = ref(false);
   background: #F76F72
   display: inline-block
   animation: cursor-blink 1.2s steps(2) infinite
-
+.logininput
+  padding: 10px
+  color: #F76F72 !important
+.loginbtn
+  width: 100%
+  height: 3em
+  color: #000000!important
+  background-color: #F76F72
+  font-family: 'Anek Malayalam', sans-serif
 @keyframes cursor-blink
   0%
     opacity: 0
